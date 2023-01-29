af889b37048f87010b2aac554f24b268e9f0d2a7
earl-stephens <stephensearl@hotmail.com>
2023-01-28 21:17:45 -0700

    Test and code for sum method

--- src/main/StandardDeviation.java
+++ src/main/StandardDeviation.java
@@ -5,4 +5,12 @@ public class StandardDeviation {
 	public StandardDeviation(int[] ages) {
 		this.ages = ages;
 	}
+	
+	public int sum() {
+		int sum = 0;
+		for(int age : ages) {
+			sum = sum + age;
+		}
+		return sum;
+	}
 }
--- src/test/StandardDeviationTest.java
+++ src/test/StandardDeviationTest.java
@@ -4,16 +4,27 @@ import main.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StandardDeviationTest {
+	StandardDeviation sd;
+	
+	@BeforeEach
+	void setUp() {		
+		int[] ages = {24, 30, 18, 20, 41};
+		sd = new StandardDeviation(ages);
+	}
 
 	@Test
 	void testForStandardDeviationObject() {
-		int[] ages = {24, 30, 18, 20, 41};
-		StandardDeviation sd = new StandardDeviation(ages);
 		
 		Assert.assertNotNull(sd);
 	}
-
+	
+	@Test
+	void testForSumOfIntegers() {
+		
+		Assert.assertEquals(133, sd.sum());
+	}
 }
